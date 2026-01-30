7c02f32bae24c239e0f58399cec6060174dcfc06
prince-rebel <djakaridjatraore1997@gmail.com>
2026-01-30 18:44:47 +0000

    add

--- theresidence_api_true/theresidence_api/models/pos_category.py
+++ theresidence_api_true/theresidence_api/models/pos_category.py
@@ -17,15 +17,40 @@ class PosCategory(models.Model):
                 vals['x_tr_uuid'] = str(uuid.uuid4())
         return super().create(vals_list)
 
+    # def to_category_api_dict(self):
+    #     self.ensure_one()
+    #     base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+    #     image_url = f"{base_url}/web/image/pos.category/{self.id}/image_128" if self.image_128 else ''
+        
+    #     return {
+    #         'id': self.x_tr_uuid or str(self.id),
+    #         'kindId': self.x_tr_menu_kind_id.x_uuid if self.x_tr_menu_kind_id else '',
+    #         'kindName': self.x_tr_menu_kind_id.name if self.x_tr_menu_kind_id else '',
+    #         'name': self.name or '',
+    #         'imageUrl': image_url,
+    #         'sortOrder': self.sequence or 0
+    #     }
     def to_category_api_dict(self):
         self.ensure_one()
         base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-        image_url = f"{base_url}/web/image/pos.category/{self.id}/image_128" if self.image_128 else ''
-        
+
+        image_url = (
+            f"{base_url}/web/image/pos.category/{self.id}/image_128"
+            if self.image_128 else ''
+        )
+
+        # 🔥 Trouver la catégorie racine
+        root = self
+        while root.parent_id:
+            root = root.parent_id
+
         return {
             'id': self.x_tr_uuid or str(self.id),
-            'kindId': self.x_tr_menu_kind_id.x_uuid if self.x_tr_menu_kind_id else '',
-            'kindName': self.x_tr_menu_kind_id.name if self.x_tr_menu_kind_id else '',
+
+            # ✅ si racine → elle-même
+            'kindId': root.x_tr_uuid or str(root.id),
+            'kindName': root.name or '',
+
             'name': self.name or '',
             'imageUrl': image_url,
             'sortOrder': self.sequence or 0
