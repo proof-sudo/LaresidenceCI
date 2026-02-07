60b4bfe1f80195638b36131a920fbd5a9b1b4af4
prince-rebel <djakaridjatraore1997@gmail.com>
2026-02-07 01:50:32 +0000

    add

--- webhook_manager/models/webhook_models.py
+++ webhook_manager/models/webhook_models.py
@@ -1,27 +1,27 @@
-# -*- coding: utf-8 -*-
-from odoo import models
+# # -*- coding: utf-8 -*-
+# from odoo import models
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-    _inherit = ["sale.order", "webhook.mixin"]
+# class SaleOrder(models.Model):
+#     _inherit = "sale.order"
+#     _inherit = ["sale.order", "webhook.mixin"]
 
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-    _inherit = ["res.partner", "webhook.mixin"]
+# class ResPartner(models.Model):
+#     _inherit = "res.partner"
+#     _inherit = ["res.partner", "webhook.mixin"]
 
-class SaleSubscription(models.Model):
-    _inherit = "sale.subscription"
-    _inherit = ["sale.subscription", "webhook.mixin"]
+# class SaleSubscription(models.Model):
+#     _inherit = "sale.subscription"
+#     _inherit = ["sale.subscription", "webhook.mixin"]
 
-# Pour les modèles de votre version initiale :
-class ProductProduct(models.Model):
-    _inherit = "product.product"
-    _inherit = ["product.product", "webhook.mixin"]
+# # Pour les modèles de votre version initiale :
+# class ProductProduct(models.Model):
+#     _inherit = "product.product"
+#     _inherit = ["product.product", "webhook.mixin"]
 
-class StockPicking(models.Model):
-    _inherit = "stock.picking"
-    _inherit = ["stock.picking", "webhook.mixin"]
+# class StockPicking(models.Model):
+#     _inherit = "stock.picking"
+#     _inherit = ["stock.picking", "webhook.mixin"]
 
-class AccountMove(models.Model):
-    _inherit = "account.move"
-    _inherit = ["account.move", "webhook.mixin"]
+# class AccountMove(models.Model):
+#     _inherit = "account.move"
+#     _inherit = ["account.move", "webhook.mixin"]
