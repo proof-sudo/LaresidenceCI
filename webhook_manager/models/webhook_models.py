94c74a8c476405f030f15dfeb0f3c1a63507a896
prince-rebel <djakaridjatraore1997@gmail.com>
2026-02-07 01:06:44 +0000

    add

--- webhook_manager/models/webhook_models.py
+++ webhook_manager/models/webhook_models.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import models,api
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -11,38 +11,25 @@ class SaleOrderWebhookProxy(models.Model):
     _inherit = "sale.order"
     _description = "Proxy pour webhooks Sale Order"
 
-    def write(self, vals):
-        res = super().write(vals)
-        for rec in self:
-            _logger.info(f"[Webhook Proxy] Sale Order write: {vals}")
-            # Appel manuel du mixin pour envoyer le webhook
-            self.env['webhook.mixin']._send_webhook(rec, "write", rec.read()[0], changed_fields=vals)
-        return res
-
+    @api.model
     def create(self, vals):
         record = super().create(vals)
         _logger.info(f"[Webhook Proxy] Sale Order create: {vals}")
         self.env['webhook.mixin']._send_webhook(record, "create", record.read()[0])
         return record
 
+    def write(self, vals):
+        res = super().write(vals)
+        for rec in self:
+            _logger.info(f"[Webhook Proxy] Sale Order write: {vals}")
+            self.env['webhook.mixin']._send_webhook(rec, "write", rec.read()[0], changed_fields=vals)
+        return res
+
     def unlink(self):
         for rec in self:
             _logger.info(f"[Webhook Proxy] Sale Order unlink")
             self.env['webhook.mixin']._send_webhook(rec, "unlink", rec.read()[0])
         return super().unlink()
-    
-class ProductCategoryWebhookProxy(models.Model):
-    _name = "product.category.webhook.proxy"
-    _inherit = "product.category"
-    _description = "Proxy pour envoyer webhook Product Category"
-
-    def write(self, vals):
-        res = super().write(vals)
-        for rec in self:
-            _logger.info(f"[Webhook Proxy] Product Category write: {vals}")
-            # Appel à _send_webhook via mixin manuel
-            self.env['webhook.mixin']._send_webhook(rec, "write", rec.read()[0], changed_fields=vals)
-        return res
 
 # POS Category
 class PosCategoryWebhookProxy(models.Model):
