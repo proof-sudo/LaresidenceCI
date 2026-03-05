4523b003dc3ba1df1894bc3b8ef9db7a51af21f1
prince-rebel <djakaridjatraore1997@gmail.com>
2026-03-05 08:43:25 +0000

    modification reservation

--- theresidence_api_true/theresidence_api/static/src/js/ReservationButton.js
+++ theresidence_api_true/theresidence_api/static/src/js/ReservationButton.js
@@ -1,47 +1,10 @@
 /** @odoo-module */
 
-import { Component, useState, onMounted } from "@odoo/owl";
-import { registry } from "@web/core/registry";
-import { usePos } from "@point_of_sale/app/store/pos_hook";
-import { useService } from "@web/core/utils/hooks";
-import { Navbar } from "@point_of_sale/app/navbar/navbar";
 import { patch } from "@web/core/utils/patch";
-
-export class ReservationButton extends Component {
-    static template = "theresidence_api.ReservationButton";
-
-    setup() {
-        this.pos = usePos();
-        this.orm = useService("orm");
-        this.state = useState({ pendingCount: 0 });
-        onMounted(() => this.loadCount());
-    }
-
-    async loadCount() {
-        try {
-            const result = await this.orm.call("sale.order", "get_pos_reservations", []);
-            this.state.pendingCount = result.filter((r) => r.status === "PENDING").length;
-        } catch (_) {
-            // silencieux si erreur de chargement du compteur
-        }
-    }
-
-    get pendingCount() {
-        return this.state.pendingCount;
-    }
-
-    onClick() {
-        this.pos.showScreen("ReservationScreen");
-    }
-}
+import { Navbar } from "@point_of_sale/app/navbar/navbar";
 
 patch(Navbar.prototype, {
-    get reservationButton() {
-        return ReservationButton;
+    showReservationScreen() {
+        this.pos.showScreen("ReservationScreen");
     },
-});
-
-registry.category("pos_navbar_buttons").add("ReservationButton", {
-    component: ReservationButton,
-    condition: () => true,
 });
