52e58f118e3b99a24bbd2ed5512ad3215ebc26ae
prince-rebel <djakaridjatraore1997@gmail.com>
2026-03-29 07:14:34 +0000

    fix(acces): supprimer @depends impossible sur res.users - calcul à chaque accès

--- theresidence_api_true/theresidence_api/models/res_users.py
+++ theresidence_api_true/theresidence_api/models/res_users.py
@@ -48,7 +48,6 @@ class ResUsers(models.Model):
     def _get_tr_group(self, xmlid):
         return self.env.ref(xmlid, raise_if_not_found=False)
 
-    @api.depends('id')
     def _compute_tr_groups(self):
         groups = {
             fname: self._get_tr_group(xmlid)
