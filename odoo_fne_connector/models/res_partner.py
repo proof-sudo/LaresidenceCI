7b006b037377802d9b3d98c162722a3f8bc85b91
proof-sudo <dtraore@neuronestech.com>
2026-05-27 14:05:14 +0000

    mobile retiré

--- odoo_fne_connector/models/res_partner.py
+++ odoo_fne_connector/models/res_partner.py
@@ -15,8 +15,7 @@ class InheritResPartner(models.Model):
            "B2G : Institution gouvernementale\n"
            "B2C : Particulier"
        ))
-    mobile= fields.Char(string='Mobile')
-
+    
     regimeFiscal = fields.Selection([
         ('tva', 'TVA normal de 18%'),
         ('tvab', 'TVA réduit de 9%'),
